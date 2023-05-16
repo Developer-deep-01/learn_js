6123551ea2763aa7ec16bf78fddc4271c20b98f8
Deepansh Gangwar <92908151+Developer-deep-01@users.noreply.github.com>
2023-05-16 14:43:20 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+// alert("3 + 3") we are using node js not a browser
+console.log(5 + 5);
+// code readability always high 
+console.log("coder Deep");
+
+let name = "Deepansh Gangwar "
+let age = 18
+let isloggedIn = false
+
+// number => 2 to 53 
+// bigint 
+// string =>"
+// Boolean => true/false 
+// null => standlone value 
+//  undefined =>
+// symbols => unique 
+
+
+
+//  Objects 
+
+
+console.table([name, age , isloggedIn])
+
+console.log(typeof "deepansh" , null , age , );               // string , null , function 
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Deepansh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Deepansh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Deepansh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *********************** Operations *********************
